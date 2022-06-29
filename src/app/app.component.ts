80f5b5faddd7e2f1b332d77efe28080b67c73bea
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2022-06-29 10:06:34 -0500

    Implementacion del primeNg module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  
+  nombre: string  = 'feRnAnDo heRRera';
+
+  valor: number = 1000;
+
+
+  mostrarnombre(){
+    console.log(this.nombre);
+  }
+
 }
